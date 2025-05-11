bf5a92b696e0a9a38d64e5717ae049e2c3e03ddb
FakeCoder01 <ayonsamajder@gmail.com>
2025-05-11 16:08:44 +0700

    /api/admin/ fixed

--- backend/specialworld/urls.py
+++ backend/specialworld/urls.py
@@ -20,6 +20,12 @@ from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Special World API",
@@ -33,8 +39,12 @@ urlpatterns = [
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
+    path('api/admin/', admin.site.urls, name="admin_panel_api"),
     path('admin/', admin.site.urls, name="admin_panel"),
+
 
     path('educators/', include('educators.urls'), name="educators_api"),
     path('children/', include('children.urls'), name="children_api"),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
